7502390cd4ae023ebf06552446f5b46571c6342f
Adhox <30460179+adhox@users.noreply.github.com>
2017-09-13 21:19:21 -0700

    update rows.go

--- rows.go
+++ rows.go
@@ -1,19 +1,21 @@
 package panel
 
-
+// Rows ...
 func (p Panel) Rows(points ...int) Panel {
-	l := len(points)
-	switch {
-	case l == 1:
-		return Rows(p, points[0])
-	case l > 1:
-		return Rows(p, points[0], points[1])
-	default:
-		return Rows(p)
-	}
+	// l := len(points)
+	// switch {
+	// case l == 1:
+	// 	return Rows(p, points[0])
+	// case l > 1:
+	// 	return Rows(p, points[0], points[1])
+	// default:
+	// 	return Rows(p)
+	// }
+	return Rows(p, points...)
 
 }
 
+// Rows ...
 func Rows(p Panel, points ...int) Panel {
 	var start, end int
 	tempPanel := Panel{}
@@ -31,7 +33,7 @@ func Rows(p Panel, points ...int) Panel {
 		end = -1
 	}
 
-	for header, _ := range p {
+	for header := range p {
 		c := []interface{}{}
 		if end == -1 {
 			for row, val := range p[header] {
@@ -55,4 +57,31 @@ func Rows(p Panel, points ...int) Panel {
 	}
 
 	return tempPanel
+}
+
+// Head ...
+func (p Panel) Head(i ...int) Panel {
+	return Head(p, i...)
+}
+
+// Head ...
+func Head(p Panel, i ...int) Panel {
+	if len(i) == 0 {
+		return p.Rows(0, 4)
+	}
+	return p.Rows(0, i[0]-1)
+}
+
+// Tail ...
+func (p Panel) Tail(i ...int) Panel {
+	return Tail(p, i...)
+}
+
+// Tail ...
+func Tail(p Panel, i ...int) Panel {
+	ln := p.Size().Length
+	if len(i) == 0 {
+		return p.Rows(ln-5, ln)
+	}
+	return p.Rows(ln-i[0], ln)
 }
