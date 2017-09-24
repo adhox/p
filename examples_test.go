12712aff13fd3df0dae45479395e235ae9d217e2
Adhox <30460179+adhox@users.noreply.github.com>
2017-09-23 20:08:32 -0700

    Add files via upload

--- examples_test.go
+++ examples_test.go
@@ -2,6 +2,9 @@ package panel
 
 import "fmt"
 
+// print string
+//
+// This example adds a permission for an S3 bucket to invoke a Lambda function.
 func ExampleString_unload() {
 	// Here's an example to unload the data frame.
 	fmt.Printf("Hi")
