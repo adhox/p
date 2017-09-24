8116d9146b9540e936241056e92ce2bc440bc63c
Adhox <30460179+adhox@users.noreply.github.com>
2017-09-24 08:07:41 -0700

    Add files via upload

--- doc.go
+++ doc.go
@@ -1,6 +1,18 @@
-// Package panel is a data analysis framework that abstracts much of the
+// Package panel provides a data analysis framework that abstracts much of the
 // handling and manipulating of large datasets.  API is still not stable,
 // use cautiously.
+//
+//
+// Loading Data
+// Panel loads data from several formats...
+//
+//
+// Unloading Data
+// Panel unloads data to several formats...
+//
+//
+//
+//
 package panel
 
 // http://elliot.land/post/godoc-tips-tricks
